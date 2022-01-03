0b6bf8c84128e25a5b3580049a68212d9d3ef56f
ljubank123 <ljubank123@users.noreply.github.com>
2022-01-03 16:41:37 +0100

    Update PrvaAktivnost.java

--- PrvaAktivnost.java
+++ PrvaAktivnost.java
@@ -6,16 +6,14 @@ import android.view.View;
 import android.os.Bundle;
 import android.content.Intent;
 
-
 public class PrvaAktivnost extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.aktivnost_prva);
     }
-
-  
 
 
     public void idinaprojekat (View view){
